037d0763b24dd870ccb3d0efc53ada3068688a69
Callum Jackson <callumj@uk.ibm.com>
2020-03-20 02:13:04 -0700

    .

--- resources/kubernetes/buildldapmq/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "DELETING NAMESPACE THAT REMOVES THE MAJORITY"
-oc delete namespace ldap
-
-echo "DELETE CLUSTER IMAGE POLICY"
-oc delete ClusterImagePolicy openldap
-
-echo "COMPLETED"
