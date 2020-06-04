c8ea42015b983f77d3c904bc54398a6ef450c0a4
Callum P Jackson <CALLUMJ@uk.ibm.com>
2020-06-04 21:54:53 +0100

    Update installbuild.sh

--- resources/kubernetes/buildldapmq/installbuild.sh
+++ resources/kubernetes/buildldapmq/installbuild.sh
@@ -1,5 +1,4 @@
 oc create -f createimagestream.yaml -n mq
-oc create -f CustomImagePolicy.yaml -n mq
 oc create -f buildconfig.yaml -n mq
 oc start-build mqldap -n mq
 oc create -f mqcert.yaml -n mq
