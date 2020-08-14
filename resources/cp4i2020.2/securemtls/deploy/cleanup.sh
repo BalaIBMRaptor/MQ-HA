e173edf6b71089149368b25e3d990de029c494f6
callum <callumj@uk.ibm.com>
2020-08-14 15:27:44 +0100

    Correct for 9.2

--- resources/cp4i2020.2/securemtls/deploy/cleanup.sh
+++ resources/cp4i2020.2/securemtls/deploy/cleanup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-export TARGET_NAMESPACE=mqquickstart
+export TARGET_NAMESPACE=cp4i
 
 oc delete queuemanager mtlsqm -n $TARGET_NAMESPACE
 oc delete secret mqcert -n $TARGET_NAMESPACE
 oc delete route mq-mtls-traffic-operator-test -n $TARGET_NAMESPACE
-oc delete pvc data-mtlsqm-ibm-mq-0 -n $TARGET_NAMESPACE
+oc delete configmap mtlsmqsc -n $TARGET_NAMESPACE
 
--- resources/cp4i2020.2/securemtls/deploy/install.sh
+++ resources/cp4i2020.2/securemtls/deploy/install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export TARGET_NAMESPACE=mqquickstart
+export TARGET_NAMESPACE=cp4i
 export QM_KEY=$(cat ../createcerts/server.key | base64 | tr -d '\n')
 export QM_CERT=$(cat ../createcerts/server.crt | base64 | tr -d '\n')
 export APP_CERT=$(cat ../createcerts/application.crt | base64 | tr -d '\n')
