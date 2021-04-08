22cbdc80686744546d287de92573277cfa524005
Callum Jackson <callumj@uk.ibm.com>
2021-04-08 17:21:41 +0100

    Corrections based on comments from Ant

--- resources/cp4i2020.2/uniformcluster/test/rClient.sh
+++ resources/cp4i2020.2/uniformcluster/test/rClient.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export MQCCDTURL="${DIR}/ccdt_generated.json"
 export MQSSLKEYR="${DIR}/key"
 
-export ROOTURL="$(oc get routes ucqm1-ibm-mq-qm -n cp4i -o jsonpath='{.status.ingress[].routerCanonicalHostname}')"
+export ROOTURL="$(oc get IngressController default -n openshift-ingress-operator -o jsonpath='{.status.domain}')"
 ( echo "cat <<EOF" ; cat ccdt_template.json ; echo EOF ) | sh > ccdt_generated.json
 
 CCDT_NAME=${2:-"*ANY_QM"}
