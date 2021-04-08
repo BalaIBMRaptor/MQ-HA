38c446c482b482454a147c27b830bfe790367ad2
Callum Jackson <callumj@uk.ibm.com>
2021-04-08 20:48:01 +0100

    Update to use Kubernetes Services for Uniform Cluster internal communication

--- resources/cp4i2020.2/uniformcluster/deploy/cleanup.sh
+++ resources/cp4i2020.2/uniformcluster/deploy/cleanup.sh
@@ -5,6 +5,7 @@ export TARGET_NAMESPACE=${1:-"cp4i"}
 oc delete queuemanager ucqm1 -n $TARGET_NAMESPACE
 oc delete queuemanager ucqm2 -n $TARGET_NAMESPACE
 oc delete secret uniformclustercert -n $TARGET_NAMESPACE
+oc delete configmap uniformcluster -n $TARGET_NAMESPACE
 oc delete configmap uniformclusterqm1 -n $TARGET_NAMESPACE
 oc delete configmap uniformclusterqm2 -n $TARGET_NAMESPACE
 oc delete pvc data-ucqm1-ibm-mq-0 -n $TARGET_NAMESPACE
