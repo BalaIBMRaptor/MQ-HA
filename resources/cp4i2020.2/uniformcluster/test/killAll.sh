c55009897954095d68d8a9037723365a9eb1681c
Callum Jackson <CALLUMJ@uk.ibm.com>
2021-08-16 13:47:16 +0100

    Update killAll.sh

--- resources/cp4i2020.2/uniformcluster/test/killAll.sh
+++ resources/cp4i2020.2/uniformcluster/test/killAll.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-APPNAME=${1:-amqsphac}
-
-kill $(ps -e | grep $APPNAME | awk '{print $1}')
-kill $(ps -e | grep amqsghac | awk '{print $1}')
+kill $(ps -e | grep -v grep | grep amqsphac | awk '{print $1}')
+kill $(ps -e | grep -v grep | grep amqsghac | awk '{print $1}'
 
